ef06bc702befd0c32c966d58bb273a592f44ddc9
RoboCoderJet <s.antonov@live.com>
2023-12-27 16:47:12 +0000

    Configured rules.mk

--- keyboards/thekeeb/rules.mk
+++ keyboards/thekeeb/rules.mk
@@ -1,1 +1,10 @@
-# This file intentionally left blank
+# Copyright 2024 Robo Coder Jet
+# Build Options
+#   change yes to no to disable
+#
+
+# Activate Repeat key and Key Lock functionalities
+REPEAT_KEY_ENABLE = yes
+KEY_LOCK_ENABLE = yes
+
+CONVERT_TO=rp2040_ce
